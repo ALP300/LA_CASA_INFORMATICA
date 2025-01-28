d424cdbcd106f579ada28dbc0db59359fe58b6a0
ALP300 <aitorpalomino1@gmail.com>
2025-01-28 16:35:17 -0500

    Realizar commit

--- src/services/conexion.mjs
+++ src/services/conexion.mjs
@@ -3,10 +3,10 @@ const { Client }= pg;
 
 
 const config={
-    user: 'db_cine_user',
-    host: 'dpg-cqsmhjdumphs73c1ord0-a.oregon-postgres.render.com',
-    database: 'db_cine',
-    password: 'DYjI59OSB73eQnh7JTQLBiFa6BItjLfe',
+    user: 'db_lacasainformatica_162e_user',
+    host: 'dpg-cuckp5tds78s73a1mom0-a.oregon-postgres.render.com',
+    database: 'db_lacasainformatica_162e',
+    password: '4wgpvnOqOzY5V3PQLqR8SHmbbKlDy2k7',
     port: 5432,
     ssl: {
         rejectUnauthorized: false
@@ -20,32 +20,5 @@ export async function Conectar() {
         console.log("Conectado a la base de datos")
     }catch(error){
         console.log(error)
-    }
-}
-
-export async function TraerPeliculas() {
-    const cliente= new Client(config)
-    try{
-        await cliente.connect()
-        const res= await cliente.query('SELECT * FROM peliculas')
-        return res.rows
-    }catch(error){
-        console.log(error)
-    }
-}
-
-export async function guardarPelicula(pelicula) {
-    const cliente= new Client(config)
-    try{
-        await cliente.connect()
-        const queryPeli= 'INSERT INTO peliculas (titulo,descripcion,url) VALUES($1,$2,$3)';
-        const values=[pelicula.titulo, pelicula.descripcion, pelicula.url];
-        await cliente.query(queryPeli, values)
-        return true
-    }catch(error){
-        console.log(error)
-        return false
-    }finally{
-        await cliente.end()
     }
 }
