0a50b6080702eeffee954fde9b515cf48c2fc7b8
ALP300 <aitorpalomino1@gmail.com>
2025-01-30 16:25:08 -0500

    Actualizar api

--- src/index.js
+++ src/index.js
@@ -1,34 +1,27 @@
 import express from 'express';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
-import cors from 'cors';
-import indexRoute from './routes/index.js';
+import router from './routes/index.js';
 import { Conectar } from './public/services/conexion.js';
+import cors from 'cors';
 
 const app = express();
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.json());
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Configurar CORS para permitir solicitudes desde el origen específico
-const corsOptions = {
-  origin: 'https://la-casa-informatica-6.onrender.com',
-  optionsSuccessStatus: 200
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 
-app.use(indexRoute);
 
-// Servir archivos estáticos
+app.use(router);
 app.use(express.static(join(__dirname, 'public')));
 
-// Conectar a la base de datos
-Conectar();
 
-// Iniciar el servidor
 app.listen(3000, () => {
-  console.log("El servidor está siendo escuchado por el puerto 3000");
+    console.log('Server is running on http://localhost:3000');
 });
+
+Conectar();
